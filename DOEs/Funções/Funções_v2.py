83b7d1983d228e78b0a6a649e49ea8a63b755c7e
Vinicius de Moraes Menezes <132612286+Luckiestaxe@users.noreply.github.com>
2024-03-14 12:20:28 -0300

    Update Funções_v2.py

--- DOEs/Funções/Funções_v2.py
+++ DOEs/Funções/Funções_v2.py
@@ -10,67 +10,64 @@ class Data: Informações sobre data encontrada num DOE
 
 #Busca por datas por extenso e resumida alocando em um vetor com todas as datas e identificação da data por documento
 def find_date(pdf_files, datas):
-  leitor = PyPDF2.PdfReader(pdf_files)
-  for i in range(0,len(leitor.pages)):
-    sentences = sent_tokenize(leitor.pages[i].extract_text())
-    for S in range(0,len(sentences)):
-      TOKEN = word_tokenize(sentences[S])
-      for j in range(0,len(TOKEN)):
-        TOKEN[j].lower()
-        if(TOKEN[j].isdigit() and j+4<len(TOKEN)): #Caso data por extenso exemplo: 25 de dezembro de 2023
-          j=j+1
-          if(TOKEN[j] == 'de'):
+  with pdfplumber.open(pdf_files) as leitor:
+    for i in range(0,len(leitor.pages)):
+      sentences = sent_tokenize(leitor.pages[i].extract_text())
+      for S in range(0,len(sentences)):
+        TOKEN = word_tokenize(sentences[S])
+        for j in range(0,len(TOKEN)):
+          TOKEN[j].lower()
+          if(TOKEN[j].isdigit() and j+4<len(TOKEN)): #Caso data por extenso exemplo: 25 de dezembro de 2023
             j=j+1
-            if(TOKEN[j] == 'janeiro' or TOKEN[j] == 'fevereiro' or TOKEN[j] == 'março' or TOKEN[j] == 'abril' or TOKEN[j] == 'maio' or TOKEN[j] == 'junho' or TOKEN[j] == 'julho' or TOKEN[j] == 'agosto' or TOKEN[j] == 'setembro' or TOKEN[j] == 'outubro' or TOKEN[j] == 'novembro' or TOKEN[j] == 'dezembro'):
+            if(TOKEN[j] == 'de'):
               j=j+1
-              if(TOKEN[j] == 'de'):
+              if(TOKEN[j] == 'janeiro' or TOKEN[j] == 'fevereiro' or TOKEN[j] == 'março' or TOKEN[j] == 'abril' or TOKEN[j] == 'maio' or TOKEN[j] == 'junho' or TOKEN[j] == 'julho' or TOKEN[j] == 'agosto' or TOKEN[j] == 'setembro' or TOKEN[j] == 'outubro' or TOKEN[j] == 'novembro' or TOKEN[j] == 'dezembro'):
                 j=j+1
-                if(TOKEN[j].isdigit()):
-                  datas.append(TOKEN[j-4]+' '+TOKEN[j-3]+' '+TOKEN[j-2]+' '+TOKEN[j-1]+' '+TOKEN[j])
-                  pdf_files = pdf_files.replace('/content/DOEs/','')
-                  datas[-1] = Data(datas[-1],pdf_files,i,S)
-
-        elif(TOKEN[j].count('/')>=2): #Caso data resumida exemplo: DD/MM/YYYY
-          x=0 #posição na string
-          n=0 #quantidade de números
-          b=0 #quantidade de /
-          while(x<len(TOKEN[j])):
-            if(TOKEN[j][x].isnumeric()):
-              n+=1
-            elif(TOKEN[j][x] == '/' and ((n==2 and b==0) or (n==4 and b==1))):
-              b+=1
-            elif(TOKEN[j][x].isnumeric()==False and TOKEN[j][x] != '/'):
-              TOKEN[j] = TOKEN[j].replace(TOKEN[j][x],'')
-              continue
-            elif(TOKEN[j][x] == '/' and b==2):
-              temp1 = TOKEN[j].split('/')
-              temp2  = temp1[0]
-              for f in range(1,len(temp1)-1):
-                temp2 = temp2 + '/' + temp1[f]
-              TOKEN[j] = temp2
-            x=x+1
-          if(n==8 and b==2):
-            datas.append(TOKEN[j])
-            pdf_files = pdf_files.replace('/content/DOEs/','')
-            datas[-1] = Data(datas[-1],pdf_files,i,S)
-
-        elif(TOKEN[j].count('.')==2 and TOKEN[j].count('/')==0 and TOKEN[j].count('-')==0): #Caso data formato DD.MM.YYYY
-          C=0
-          N=0
-          while(C<len(TOKEN[j])):
-            if(TOKEN[j][C].isnumeric()):
-              C+=1
-              N+=1
-            elif(TOKEN[j][C]=='.' and N%3!=0):
-              C+=1
-              N+=1
-            else:
-              TOKEN[j] = TOKEN[j].replace(TOKEN[j][C],'')
-          if(N==10):
-            datas.append(TOKEN[j])
-            pdf_files = pdf_files.replace('/content/DOEs/','')
-            datas[-1] = Data(datas[-1], pdf_files, i, S)
-
+                if(TOKEN[j] == 'de'):
+                  j=j+1
+                  if(TOKEN[j].isdigit()):
+                    datas.append(TOKEN[j-4]+' '+TOKEN[j-3]+' '+TOKEN[j-2]+' '+TOKEN[j-1]+' '+TOKEN[j])
+                    pdf_files = pdf_files.replace('/content/DOEs/','')
+                    datas[-1] = Data(datas[-1],pdf_files,i,S)
+          elif(TOKEN[j].count('/')>=2): #Caso data resumida exemplo: DD/MM/YYYY
+            x=0 #posição na string
+            n=0 #quantidade de números
+            b=0 #quantidade de /
+            while(x<len(TOKEN[j])):
+              if(TOKEN[j][x].isnumeric()):
+                n+=1
+              elif(TOKEN[j][x] == '/' and ((n==2 and b==0) or (n==4 and b==1))):
+                b+=1
+              elif(TOKEN[j][x].isnumeric()==False and TOKEN[j][x] != '/'):
+                TOKEN[j] = TOKEN[j].replace(TOKEN[j][x],'')
+                continue
+              elif(TOKEN[j][x] == '/' and b==2):
+                temp1 = TOKEN[j].split('/')
+                temp2  = temp1[0]
+                for f in range(1,len(temp1)-1):
+                  temp2 = temp2 + '/' + temp1[f]
+                TOKEN[j] = temp2
+              x=x+1
+            if(n==8 and b==2):
+              datas.append(TOKEN[j])
+              pdf_files = pdf_files.replace('/content/DOEs/','')
+              datas[-1] = Data(datas[-1],pdf_files,i,S)
+          elif(TOKEN[j].count('.')==2 and TOKEN[j].count('/')==0 and TOKEN[j].count('-')==0): #Caso data formato DD.MM.YYYY
+            C=0
+            N=0
+            while(C<len(TOKEN[j])):
+              if(TOKEN[j][C].isnumeric()):
+                C+=1
+                N+=1
+              elif(TOKEN[j][C]=='.' and N%3!=0):
+                C+=1
+                N+=1
+              else:
+                TOKEN[j] = TOKEN[j].replace(TOKEN[j][C],'')
+            if(N==10):
+              datas.append(TOKEN[j])
+              pdf_files = pdf_files.replace('/content/DOEs/','')
+              datas[-1] = Data(datas[-1], pdf_files, i, S)  
   return datas
 
 #Extrai palavras em negrito de um PDF e separa órgãos caso estejam concatenados a qualquer outra palavras
